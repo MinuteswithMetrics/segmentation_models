71a6a2f0ff08985e9f1ef3c416740df50acda414
MinuteswithMetrics <minuteswithmetrics@gmail.com>
2019-07-18 11:20:53 -0500

    Update model.py

--- segmentation_models/nestnet/model.py
+++ segmentation_models/nestnet/model.py
@@ -10,7 +10,63 @@ old_args_map = {
     'input_tensor': None,  # removed
 }
 
+DEFAULT_FEATURE_LAYERS = {
 
+    # List of layers to take features from backbone in the following order:
+    # (x16, x8, x4, x2, x1) - `x4` mean that features has 4 times less spatial
+    # resolution (Height x Width) than input image.
+
+    # VGG
+    'vgg16': ('block5_conv3', 'block4_conv3', 'block3_conv3', 'block2_conv2', 'block1_conv2'),
+    'vgg19': ('block5_conv4', 'block4_conv4', 'block3_conv4', 'block2_conv2', 'block1_conv2'),
+
+    # ResNets
+    'resnet18': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'resnet34': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'resnet50': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'resnet101': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'resnet152': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+
+    # ResNeXt
+    'resnext50': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'resnext101': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+
+    # Inception
+    'inceptionv3': (228, 86, 16, 9),
+    'inceptionresnetv2': (594, 260, 16, 9),
+
+    # DenseNet
+    'densenet121': (311, 139, 51, 4),
+    'densenet169': (367, 139, 51, 4),
+    'densenet201': (479, 139, 51, 4),
+
+    # SE models
+    'seresnet18': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'seresnet34': ('stage4_unit1_relu1', 'stage3_unit1_relu1', 'stage2_unit1_relu1', 'relu0'),
+    'seresnet50': (233, 129, 59, 4),
+    'seresnet101': (522, 129, 59, 4),
+    'seresnet152': (811, 197, 59, 4),
+    'seresnext50': (1065, 577, 251, 4),
+    'seresnext101': (2442, 577, 251, 4),
+    'senet154': (6837, 1614, 451, 12),
+
+    # Mobile Nets
+    'mobilenet': ('conv_pw_11_relu', 'conv_pw_5_relu', 'conv_pw_3_relu', 'conv_pw_1_relu'),
+    'mobilenetv2': ('block_13_expand_relu', 'block_6_expand_relu', 'block_3_expand_relu', 'block_1_expand_relu'),
+    
+    # EfficientNets
+    'efficientnetb0': (169, 77, 47, 17),
+    'efficientnetb1': (246, 122, 76, 30),
+    'efficientnetb2': (246, 122, 76, 30),
+    'efficientnetb3': (278, 122, 76, 30),
+    
+    # weights are not released
+    'efficientnetb4': (342, 154, 92, 30),
+    'efficientnetb5': (419, 199, 121, 43),
+#     'efficientnetb6': (483, 231, 137, 43),
+#     'efficientnetb7': (592, 276, 166, 56),
+    
+}
 
 def Nestnet(backbone_name='vgg16',
          input_shape=(None, None, 3),
