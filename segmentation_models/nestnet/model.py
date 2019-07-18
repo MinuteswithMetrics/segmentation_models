1165e7253291ba63b8effee26b38d7b9e5c37611
MinuteswithMetrics <minuteswithmetrics@gmail.com>
2019-07-18 10:26:01 -0500

    Create model.py

---
+++ segmentation_models/nestnet/model.py
@@ -0,0 +1,78 @@
+from .builder import build_unet
+from ..utils import freeze_model
+from ..utils import legacy_support
+from ..backbones import get_backbone, get_feature_layers
+
+old_args_map = {
+    'freeze_encoder': 'encoder_freeze',
+    'skip_connections': 'encoder_features',
+    'upsample_rates': None,  # removed
+    'input_tensor': None,  # removed
+}
+
+
+
+def Nestnet(backbone_name='vgg16',
+         input_shape=(None, None, 3),
+         input_tensor=None,
+         encoder_weights='imagenet',
+         freeze_encoder=False,
+         skip_connections='default',
+         decoder_block_type='upsampling',
+         decoder_filters=(256,128,64,32,16),
+         decoder_use_batchnorm=True,
+         n_upsample_blocks=5,
+         upsample_rates=(2,2,2,2,2),
+         classes=1,
+         activation='sigmoid'):
+    """
+    Args:
+        backbone_name: (str) look at list of available backbones.
+        input_shape:  (tuple) dimensions of input data (H, W, C)
+        input_tensor: keras tensor
+        encoder_weights: one of `None` (random initialization), 
+            'imagenet' (pre-training on ImageNet), 
+            'dof' (pre-training on DoF)
+        freeze_encoder: (bool) Set encoder layers weights as non-trainable. Useful for fine-tuning
+        skip_connections: if 'default' is used take default skip connections,
+            else provide a list of layer numbers or names starting from top of model
+        decoder_block_type: (str) one of 'upsampling' and 'transpose' (look at blocks.py)
+        decoder_filters: (int) number of convolution layer filters in decoder blocks
+        decoder_use_batchnorm: (bool) if True add batch normalisation layer between `Conv2D` ad `Activation` layers
+        n_upsample_blocks: (int) a number of upsampling blocks
+        upsample_rates: (tuple of int) upsampling rates decoder blocks
+        classes: (int) a number of classes for output
+        activation: (str) one of keras activations for last model layer
+    Returns:
+        keras.models.Model instance
+    """
+
+
+
+    backbone = get_backbone(backbone_name,
+                            input_shape=input_shape,
+                            input_tensor=input_tensor,
+                            weights=encoder_weights,
+                            include_top=False)
+
+    if skip_connections == 'default':
+        skip_connections = DEFAULT_SKIP_CONNECTIONS[backbone_name]
+    # n_upsample_blocks = len(skip_connections)
+
+    model = build_nestnet(backbone,
+                       classes,
+                       skip_connections,
+                       decoder_filters=decoder_filters,
+                       block_type=decoder_block_type,
+                       activation=activation,
+                       n_upsample_blocks=n_upsample_blocks,
+                       upsample_rates=upsample_rates,
+                       use_batchnorm=decoder_use_batchnorm)
+
+    # lock encoder weights for fine-tuning
+    if freeze_encoder:
+        freeze_model(backbone)
+
+    model.name = 'nest-{}'.format(backbone_name)
+
+    return model
